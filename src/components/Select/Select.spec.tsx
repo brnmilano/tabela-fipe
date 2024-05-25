7169ddd8a6e33c3e10c468335927e7349b815148
Bruno Milano <brn.milano@hotmail.com>
2024-05-25 10:59:09 -0300

    Test creation - Addition of tests in the Button and Select components - Adding the necessary libraries for testing - Test file configuration

---
+++ src/components/Select/Select.spec.tsx
@@ -0,0 +1,52 @@
+import { render } from "@testing-library/react";
+import { ReactSelect } from "@/components/Select";
+import "@testing-library/jest-dom";
+
+describe("ReactSelect", () => {
+  const brandsOptions = [
+    { label: "Brand 1", value: 1 },
+    { label: "Brand 2", value: 2 },
+    { label: "Brand 3", value: 3 },
+  ];
+
+  it("should render with default values", () => {
+    const onSelectMock = jest.fn();
+
+    const { getByText } = render(
+      <ReactSelect
+        label="Marca"
+        name="marca"
+        placeholder="Selecione"
+        options={brandsOptions}
+        defaultValue={brandsOptions[0]}
+        onSelect={onSelectMock}
+        error
+        errorMessage="Cambo obrigatório!"
+        isDisabled
+      />
+    );
+
+    expect(getByText("Brand 1")).toBeInTheDocument();
+    expect(getByText("Marca")).toBeInTheDocument();
+  });
+
+  it("should render with required field", () => {
+    const onSelectMock = jest.fn();
+
+    const { getByText } = render(
+      <ReactSelect
+        label="Marca"
+        name="marca"
+        placeholder="Selecione"
+        options={brandsOptions}
+        defaultValue={brandsOptions[0]}
+        onSelect={onSelectMock}
+        error
+        errorMessage="Cambo obrigatório!"
+        isDisabled
+      />
+    );
+
+    expect(getByText("Cambo obrigatório!")).toBeInTheDocument();
+  });
+});
